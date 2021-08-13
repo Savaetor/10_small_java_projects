2aab8c4887383cc309e2de0f98b94ecf336e876e
Savaetor <harshd1126@gmail.com>
2021-08-13 10:49:32 +0530

    first commit

---
+++ LeapYear/src/leapyear/LeapYear.java
@@ -0,0 +1,17 @@
+
+package leapyear;
+
+public class LeapYear {
+
+    
+    public static void main(String[] args) {
+        int n=2004;
+        if(n%4==0){
+            System.out.println("The Given year is a leap year !! ");
+        }
+        else{
+            System.out.println(" This is not a leap year !! ");
+        }
+    }
+    
+}
